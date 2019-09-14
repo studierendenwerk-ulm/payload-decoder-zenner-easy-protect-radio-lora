533fd7c53f3e7426bccb73cb14f4b30b51378f1a
Aron Lanza <aron.lanza@uni-ulm.de>
2019-09-14 19:10:51 +0200

    the decoder has now been adapted to the native environment of the TTN. It works, but it is not a nice javascript code because the TTN does not support javascript code well. In particular "let".

--- decoder.js
+++ decoder.js
@@ -1,35 +1,3 @@
-// Definition of the different packet types and subtypes.
-let TYPE_SP1 = 0x1; //sent daily -> packet, max. 2 retransmissions
-let TYPE_SP9 = 0x9; // Sent immediately after first  activation and from then on every 6 months -> No retransmissions
-let SUBTYPE_SP9_1 = 0x01; // sent every month except the month of first activation -> No retransmissions
-let SUBTYPE_SP9_2 = 0x02; // sent immediately after first activation and from then on every 6 months -> No retransmissions
-let TYPE_AP1 = 0xA; //  (status code, status data): event based -> Max 5 AP packets per month, no retransmissions
-
-// Definition of head line daily value.
-/* let S_SMOKE_CHAMBER_POLLUTION_PREWARNING = 0x0800;
- let S_SMOKE_CHAMBER_POLLUTION_WARNING = 0x1000;
- let S_TEST_BUTTON_FAILUER = 0x2000;
- let S_ACCUSTIC_ALARM_FAILUER = 0x4000;
- let S_REMOVAL_DETECTION = 0x8000;
- let S_TEST_ALARM = 0x0001;
- let S_SMOKE_ALARM = 0x0002;
- let S_OBSTRUCTION_DETECTION = 0x0004;
- let S_SURROUNDING_AREA_MONITORING = 0x0008;
- let S_LED_FAILURE = 0x0010;*/
-
-// Device specific status summery definition
-let A_REMOVAL = 0x02;
-let A_BATTERY_END_OF_LIFE = 0x0C;
-let A_HORN_DRIVE_LEVEL_FAILURE = 0x16;
-let A_OBSTRUCTION_DETECTION = 0x1A;
-let A_OBJECT_IN_THE_SURROUNDING_AREA = 0x1C;
-let AP_VALUES = ["removal", "battery end of life",
-    "horn drive level failure", "obstruction detection", "object in the surrounding area"];
-let S_STATUS_SUMMERY_VALUES = ["removal", undefined,
-    "battery end of life", "acoustic alarm failure",
-    "obstruction detection", "surrounding area monitoring"];
-
-
 /**
  * decodes date (EN13757-3:2013, Annex A, data type G).
  * input: int, lower two bytes interpreted.
@@ -39,9 +7,9 @@ let S_STATUS_SUMMERY_VALUES = ["removal", undefined,
  */
 function decodeDate(bytes) {
     //TODO Handling errors with 0xFF see LoRa radio packet definitions page 15
-    let day = (bytes & 0x1F00) >> 8;
-    let month = (bytes & 0x000F);
-    let year = ((bytes & 0xE000) >> 10) | ((bytes & 0x00F0) >> 4);
+    var day = (bytes & 0x1F00) >> 8;
+    var month = (bytes & 0x000F);
+    var year = ((bytes & 0xE000) >> 10) | ((bytes & 0x00F0) >> 4);
     return "20" + year.toString() + "-" + month.toString() + "-" + day.toString();
 }
 
@@ -52,11 +20,11 @@ function decodeDate(bytes) {
  * @returns {string}
  */
 function decodeDateAndTime(bytes) {
-    let minute = (bytes & 0x3F000000) >> 24;
-    let hour = (bytes & 0x001F0000) >> 16;
-    let day = (bytes & 0x00001F00) >> 8;
-    let month = (bytes & 0x0000000F);
-    let year = ((bytes & 0x0000E000) >> 10) | ((bytes & 0x000000F0) >> 4);
+    var minute = (bytes & 0x3F000000) >> 24;
+    var hour = (bytes & 0x001F0000) >> 16;
+    var day = (bytes & 0x00001F00) >> 8;
+    var month = (bytes & 0x0000000F);
+    var year = ((bytes & 0x0000E000) >> 10) | ((bytes & 0x000000F0) >> 4);
     return "20" + year.toString() + "-"
         + month.toString() + "-"
         + day.toString() + "T"
@@ -71,14 +39,18 @@ function decodeDateAndTime(bytes) {
  * @returns {string}
  */
 function payloadRawHexToString(byteArray) {
-    let str = Array.from(byteArray, function (byte) {
+    /*var str = Array.from(byteArray, function (byte) {
         return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')*/;
+    var str;
+    str = byteArray.map(function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2)
     }).join('');
-    let strArray = str.match(/.{2}/g);
+    var strArray = str.match(/.{2}/g);
     str = strArray.map(function (value, index, array) {
         return array[index];
     });
-    let x = str.join();
+    var x = str.join();
     return x.replace(/([,])/g, " ").toLocaleUpperCase();
 }
 
@@ -88,22 +60,22 @@ function payloadRawHexToString(byteArray) {
  * @param b
  * @returns {[]}
  */
-function buildStatusSummery(a, b) {
+function buildStatusSummery(a, b,S_STATUS_SUMMERY_VALUES) {
 
-    let bin1 = parseInt(a.toString(),16).toString(2);
-    let bin2 = parseInt(b.toString(),16).toString(2);
-    let result = [];
+    var bin1 = parseInt(a.toString(), 16).toString(2);
+    var bin2 = parseInt(b.toString(), 16).toString(2);
+    var result = [];
 
-    for (let i = 0; i < bin1.length; i++) {
+    for (var i = 0; i < bin1.length; i++) {
         console.log(bin1.length);
         if (bin1[i] === "1") {
             result.push(S_STATUS_SUMMERY_VALUES[i]);
         }
     }
 
-    for (let i = 0; i < bin2.length; i++) {
+    for (var j = 0; j < bin2.length; j++) {
         if (bin2[i] === "1") {
-            result.push(S_STATUS_SUMMERY_VALUES[i]);
+            result.push(S_STATUS_SUMMERY_VALUES[j]);
         }
     }
 
@@ -117,7 +89,38 @@ function buildStatusSummery(a, b) {
  * @constructor
  */
 function Decoder(bytes, port) {
-    let obj = {};
+    // Definition of head line daily value.
+    /* let S_SMOKE_CHAMBER_POLLUTION_PREWARNING = 0x0800;
+     let S_SMOKE_CHAMBER_POLLUTION_WARNING = 0x1000;
+     let S_TEST_BUTTON_FAILUER = 0x2000;
+     let S_ACCUSTIC_ALARM_FAILUER = 0x4000;
+     let S_REMOVAL_DETECTION = 0x8000;
+     let S_TEST_ALARM = 0x0001;
+     let S_SMOKE_ALARM = 0x0002;
+     let S_OBSTRUCTION_DETECTION = 0x0004;
+     let S_SURROUNDING_AREA_MONITORING = 0x0008;
+     let S_LED_FAILURE = 0x0010;*/
+
+    // Definition of the different packet types and subtypes.
+    var TYPE_SP1 = 0x1; //sent daily -> packet, max. 2 retransmissions
+    var TYPE_SP9 = 0x9; // Sent immediately after first  activation and from then on every 6 months -> No retransmissions
+    var SUBTYPE_SP9_1 = 0x01; // sent every month except the month of first activation -> No retransmissions
+    var SUBTYPE_SP9_2 = 0x02; // sent immediately after first activation and from then on every 6 months -> No retransmissions
+    var TYPE_AP1 = 0xA; //  (status code, status data): event based -> Max 5 AP packets per month, no retransmissions
+
+    // Device specific status summery definition
+    var A_REMOVAL = 0x02;
+    var A_BATTERY_END_OF_LIFE = 0x0C;
+    var A_HORN_DRIVE_LEVEL_FAILURE = 0x16;
+    var A_OBSTRUCTION_DETECTION = 0x1A;
+    var A_OBJECT_IN_THE_SURROUNDING_AREA = 0x1C;
+    var AP_VALUES = ["removal", "battery end of life",
+        "horn drive level failure", "obstruction detection", "object in the surrounding area"];
+    var S_STATUS_SUMMERY_VALUES = ["removal", undefined,
+        "battery end of life", "acoustic alarm failure",
+        "obstruction detection", "surrounding area monitoring"];
+
+    var obj = {};
     obj.port = port;
 
     switch (bytes[0] >> 4) {
@@ -141,7 +144,7 @@ function Decoder(bytes, port) {
                     obj.packet_subtype = 1;
                     obj.dateTime = decodeDateAndTime(((bytes[1] << 24) | (bytes[2] << 16) | (bytes[3] << 8) | (bytes[4])));
                     //TODO make a function to check is the device in correct dateAndTime
-                    obj.status_interpretation = buildStatusSummery(bytes[5], bytes[6]);
+                    obj.status_interpretation = buildStatusSummery(bytes[5], bytes[6],S_STATUS_SUMMERY_VALUES);
                     obj.status_dedcoded = true;
                     obj.status_info = "payload_raw: " + payloadRawHexToString(bytes);
                     break;
